e3daeebb5cd2f74d59dc322792663207baaaa924
emazou <estefania.mazou@gmail.com>
2023-04-05 00:31:09 -0500

    Improved UI

--- app/src/main/java/com/emazou/primenumbers/MainActivity.kt
+++ app/src/main/java/com/emazou/primenumbers/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.emazou.primenumbers
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Send
 import androidx.compose.runtime.Composable
@@ -19,7 +20,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -32,28 +39,44 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@Preview
 @Composable
 fun Home(
     primeCalculatorModel: PrimeCalculatorModel = PrimeCalculatorModel()
 ){
     val textShow = remember { mutableStateOf("") }
     val listResult = remember { mutableStateOf(emptyList<Int>()) }
-    Box(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF0f172b)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = "Prime Numbers",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            fontSize = 30.sp,
+            color = Color(0xFFcfd8e5),
+            fontFamily = FontFamily.Monospace,
+        )
         Column(
-            modifier = Modifier.align(Alignment.Center)
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             InputNumber(text= "Enter an integer number", onButtonClicked = {value ->
                 val isPrime = primeCalculatorModel.isPrime(value.toInt())
-                textShow.value = "The number ${if (isPrime) "is" else "is not"} prime."
+                textShow.value = "The number ${value} ${if (isPrime) "is" else "is not"} prime."
             })
             Text(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(start = 20.dp),
-                text = textShow.value
+                    .padding(start = 20.dp)
+                    .align(Alignment.CenterHorizontally),
+                text = textShow.value,
+                fontSize = 20.sp,
+                color = Color(0xFFcfd8e5),
+                fontFamily = FontFamily.Monospace,
             )
             InputNumber(text="Enter the amount of prime numbers ", onButtonClicked = {value ->
                 listResult.value = primeCalculatorModel.giveMeXPrime(value.toInt())
@@ -71,7 +94,11 @@ fun Home(
                                 modifier = Modifier
                                     .weight(1f)
                                     .padding(10.dp),
-                                text = number.toString()
+                                text = number.toString(),
+                                fontSize = 20.sp,
+                                color = Color(0xFFcfd8e5),
+                                fontFamily = FontFamily.Monospace,
+                                fontWeight = FontWeight.Bold
                             )
                         }
                     }
@@ -81,39 +108,62 @@ fun Home(
     }
 }
 
-
 @Composable
 fun InputNumber(
     text: String = "Title",
     onButtonClicked: (String) -> Unit
 ) {
     val inputNumber = remember{ mutableStateOf("")}
+    val errorMessage = remember{ mutableStateOf("")}
     Column(
         modifier = Modifier
-            .padding(all = 20.dp)
+            .padding(20.dp)
+            .background(color = Color(0xFFF1e293c), shape = RoundedCornerShape(10))
+            .fillMaxWidth()
     ) {
         Text(
-            modifier = Modifier,
+            modifier = Modifier
+                .padding(20.dp)
+                .align(Alignment.CenterHorizontally),
             text = text,
-            fontSize = 25.sp,
+            fontSize = 20.sp,
+            color = Color(0xFFcfd8e5),
+            fontFamily = FontFamily.Monospace,
             )
-        Row(
-        ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+                .align(Alignment.CenterHorizontally),
+            horizontalArrangement = Arrangement.SpaceBetween
+                ) {
             TextField(
-                modifier = Modifier,
+                modifier = Modifier
+                    .fillMaxWidth(0.4f)
+                    .background(color = Color(0xFF3A4B61)),
                 value = inputNumber.value,
                 onValueChange = {
                     inputNumber.value = it
                 },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                textStyle = TextStyle.Default.copy(fontSize = 20.sp, color = Color(0xFFcfd8e5))
             )
             Button(
                 modifier = Modifier
-                    .padding(start = 15.dp),
+                    .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.9f),
+                colors = ButtonDefaults.buttonColors(Color(0xFF78e8bf)),
                 onClick = {
-                    onButtonClicked(inputNumber.value)
+                    if (inputNumber.value.toIntOrNull() != null){
+                        onButtonClicked(inputNumber.value)
+                    }
             }) {
-                Icon(Icons.Rounded.Send, contentDescription = "Send" )
+                Text(
+                    modifier = Modifier,
+                    text = "Calculate",
+                    fontSize = 20.sp,
+                    fontFamily = FontFamily.Monospace,
+                )
             }
         }
     }
